4aaf0cfdedd2bb24695c26bda38aaf9ffdabba82
Sumedha Chakrabarty <chakrabartysumedha774@gmail.com>
2024-11-28 16:36:07 +0530

    create sigin api route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,7 @@ import SignUp from './pages/SignUp';
 import Dashboard from './pages/Dashboard';
 import Contact from './pages/Contact';
 import Header from './components/Header';
+import Footer from './components/Footer';
 
 export default function App() {
   return (
@@ -20,7 +21,7 @@ export default function App() {
       <Route path='/dashboard' element={<Dashboard />} />
       <Route path='/contact' element={<Contact />} />
       </Routes>
-      
+      <Footer />
       </BrowserRouter>
   );
 }
