57032457296cfc87d18294499020fbb6e36f8eb3
Sumedha Chakrabarty <chakrabartysumedha774@gmail.com>
2024-11-29 18:22:00 +0530

    add admin functionality to the user

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,36 +1,36 @@
-import User from "../models/user.model.js";
-import bcryptjs from "bcryptjs";
+import User from '../models/user.model.js';
+import bcryptjs from 'bcryptjs';
 import { errorHandler } from '../utils/error.js';
 import jwt from 'jsonwebtoken';
 
 export const signup = async (req, res, next) => {
-    const { username, email, password } = req.body;
-  
-    if (
-      !username ||
-      !email ||
-      !password ||
-      username === '' ||
-      email === '' ||
-      password === ''
-    ) {
-        next(errorHandler(400, 'All fields are required'));
-    }
+  const { username, email, password } = req.body;
 
-    const hashedPassword = bcryptjs.hashSync(password, 10);
+  if (
+    !username ||
+    !email ||
+    !password ||
+    username === '' ||
+    email === '' ||
+    password === ''
+  ) {
+    next(errorHandler(400, 'All fields are required'));
+  }
 
-    const newUser = new User({
-        username,
-        email,
-        password: hashedPassword,
-      });
+  const hashedPassword = bcryptjs.hashSync(password, 10);
 
-      try {
-        await newUser.save();
-        res.json('Signup successful');
-      } catch (error) {
-        next(error);
-      }
+  const newUser = new User({
+    username,
+    email,
+    password: hashedPassword,
+  });
+
+  try {
+    await newUser.save();
+    res.json('Signup successful');
+  } catch (error) {
+    next(error);
+  }
 };
 
 export const signin = async (req, res, next) => {
